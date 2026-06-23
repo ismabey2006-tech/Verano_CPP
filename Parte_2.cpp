11e2c9712629dc93c27dca881f24273bd80f77c6
Adrian Arias <ismabey2006@gmail.com>
2026-06-23 08:30:51 -0500

    Add files via upload

---
+++ Parte_2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class RegistroTemperaturas{
+private:
+	double* lecturas;
+	int capacidad;
+	int cantidad;
+	
+	
+RegistroTemperaturas(int cap);
+
+void agregar(double temp){
+	
+}
+};
+int main(){
+}
+
